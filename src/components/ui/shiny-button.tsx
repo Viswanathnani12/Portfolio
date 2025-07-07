95f1251bc898735d65c5484ec4e2d6c684802e9c
Viswanathnani12 <viswanathnani12@gmail.com>
2025-07-07 16:19:32 +0530

    fixed shiny-button

--- src/components/ui/shiny-button.tsx
+++ src/components/ui/shiny-button.tsx
@@ -1,38 +1,46 @@
 "use client";
 
 import React from "react";
-import { motion, type AnimationProps } from "framer-motion";
-
+import { motion } from "framer-motion";
 import { cn } from "@/lib/utils";
 
-const animationProps = {
-  initial: { "--x": "100%", scale: 0.8 },
-  animate: { "--x": "-100%", scale: 1 },
-  whileTap: { scale: 0.95 },
-  transition: {
-    repeat: Infinity,
-    repeatType: "loop",
-    repeatDelay: 1,
-    type: "spring",
-    stiffness: 20,
-    damping: 15,
-    mass: 2,
-    scale: {
-      type: "spring",
-      stiffness: 200,
-      damping: 5,
-      mass: 0.5,
-    },
-  },
-} as AnimationProps;
 interface ShinyButtonProps {
   children: React.ReactNode;
   className?: string;
 }
+
 const ShinyButton = ({ children, className, ...props }: ShinyButtonProps) => {
   return (
     <motion.button
-      {...animationProps}
+      initial={{ "--x": "100%", scale: 0.8 } as any}
+      animate={{
+        "--x": "-100%",
+        scale: 1,
+        transition: {
+          "--x": {
+            repeat: Infinity,
+            repeatType: "loop" as const,
+            repeatDelay: 1,
+            type: "spring" as const,
+            stiffness: 20,
+            damping: 15,
+            mass: 2,
+          },
+          scale: {
+            type: "spring" as const,
+            stiffness: 200,
+            damping: 5,
+            mass: 0.5,
+          },
+        },
+      }}
+      whileTap={{
+        scale: 0.95,
+        transition: {
+          type: "spring" as const,
+          stiffness: 300,
+        },
+      }}
       {...props}
       className={cn(
         "relative rounded-lg px-6 py-2 font-medium backdrop-blur-xl transition-shadow duration-300 ease-in-out hover:shadow dark:bg-[radial-gradient(circle_at_50%_0%,hsl(var(--primary)/10%)_0%,transparent_60%)] dark:hover:shadow-[0_0_20px_hsl(var(--primary)/10%)]",
@@ -48,6 +56,7 @@ const ShinyButton = ({ children, className, ...props }: ShinyButtonProps) => {
       >
         {children}
       </span>
+
       <span
         style={{
           mask: "linear-gradient(rgb(0,0,0), rgb(0,0,0)) content-box,linear-gradient(rgb(0,0,0), rgb(0,0,0))",
